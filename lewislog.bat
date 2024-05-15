8452da80044aeaf0a9d13b22813b0ed6f38120a0
Peter Heesterman <Peter.Heesterman@Capgemini.com>
2024-05-15 12:56:46 +0100

    Added files back again.

---
+++ lewislog.bat
@@ -0,0 +1,11 @@
+@echo on
+set /A Instance=1
+if not "%1"=="" set /A Instance=%1
+set LewisPath=C:\Python311\scripts\
+if not "%2"=="" set LewisPath=%2
+set /A RPC_PORT=9999 + %Instance%
+set /A DEVICE_PORT=5024 + %Instance%
+set logdir=/var/log/hidenPyIoc/
+if not exist %logdir% set logdir=
+set CurrentDir=%~dp0
+%LewisPath%lewis.exe -k hidenrga interfaces -r localhost:%RPC_PORT% -p "stream: {bind_address: localhost, port: %DEVICE_PORT%}" -a %CurrentDir% > %logdir%lewis_emulator%Instance%.log 2>&1
---
+++ lewislog.sh
@@ -0,0 +1,16 @@
+declare -i Instance
+Instance=${1:-1}
+declare -i RPC_PORT
+RPC_PORT=9999+$Instance
+declare -i DEVICE_PORT
+DEVICE_PORT=5024+$Instance
+
+CurrentDir=$(dirname "$0")
+export PYTHONPATH=$CurrentDir
+
+logdir='/var/log/hidenPyIoc/'
+if [[ ! -w $logdir ]]; then
+    logdir=''
+fi
+
+lewis -k hidenrga interfaces -r localhost:$RPC_PORT -p "stream: {bind_address: localhost, port: $DEVICE_PORT}" > "$logdir"lewis_emulator$Instance.log 2>&1
