22b0f623c7b6aec878345197f2a0093db51531bd
Peter Heesterman <Peter.Heesterman@Capgemini.com>
2024-07-09 12:56:36 +0100

    Updated log file path for lewis use.

--- lewislog.bat
+++ lewislog.bat
@@ -6,6 +6,6 @@ if not "%2"=="" set LewisPath=%2
 set /A RPC_PORT=9999 + %Instance%
 set /A DEVICE_PORT=5024 + %Instance%
 set logdir=/var/log/hidenPyIoc/
-if not exist %logdir% set logdir=
+if not exist %logdir% set logdir=../../main/epics/var/log
 set CurrentDir=%~dp0
 %LewisPath%lewis.exe -k hidenrga interfaces -r localhost:%RPC_PORT% -p "stream: {bind_address: localhost, port: %DEVICE_PORT%}" -a %CurrentDir% > %logdir%lewis_emulator%Instance%.log 2>&1
